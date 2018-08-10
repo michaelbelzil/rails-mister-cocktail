0eb4f9a634fb94a15bf9ddb630450347c3824015
Michael Belzil <michaelbelzil@gmail.com>
2018-08-10 10:36:49 -0400

    catching up

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -23,7 +23,7 @@ class DosesController < ApplicationController
   def destroy
     @dose = Dose.find(params[:id])
     @dose.destroy
-    redirect_to cocktail_path(@cocktail)
+    redirect_to cocktail_path(@dose.cocktail_id)
   end
 
 private
